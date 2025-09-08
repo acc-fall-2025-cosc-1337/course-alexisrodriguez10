f04f7e084d2aafa4db516e44dea8f08cc225e0f6
alexisrodriguez10 <alexis.rodriguez10@g.austincc.edu>
2025-09-08 23:45:36 +0000

    ex01 Test Cases

--- src/examples/01_data_types/01_int/int.cpp
+++ src/examples/01_data_types/01_int/int.cpp
@@ -8,3 +8,18 @@ Function echo_variable returns the incoming parameter
 @param int-integer type
 @return return the int parameter
 */
+
+int echo_variable(int num) {
+    return num;
+}
+
+/*
+Function add_numbers returns the sum of two integers
+
+@param int num1 - first integer
+@param int num2 - second integer
+@return sum of num1 and num2
+*/
+int add_numbers(int num1, int num2) {
+    return num1 + num2;
+}
--- src/examples/01_data_types/01_int/int.h
+++ src/examples/01_data_types/01_int/int.h
@@ -1,2 +1,3 @@
 //Create prototype for void function echo_variable with one int parameter
 int echo_variable(int num);
+
